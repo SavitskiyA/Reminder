3e6f2d3d1e39865b429fea66b4968d9a6e16912f
Andrey <savitskiy.andrey@gmail.com>
2019-03-09 17:34:42 +0200

    Reminder channel title has been improved.

--- app/src/main/java/com/savitskiy/reminder/notifications/impl/PushNotificationChannel.kt
+++ app/src/main/java/com/savitskiy/reminder/notifications/impl/PushNotificationChannel.kt
@@ -7,5 +7,5 @@ import com.savitskiy.reminder.R
  * Created by andrey on 09,March,2019
  */
 sealed class PushNotificationChannel(val channelId: String, @StringRes val titleResource: Int) {
-    class Reminder : PushNotificationChannel("reminder_id", R.string.reminder_channel)
+    class Reminder : PushNotificationChannel("reminder_id", R.string.reminder_channel_title)
 }
