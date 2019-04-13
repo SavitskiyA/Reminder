d60054e8b77b57c4e6f4097e9d42f349231d6308
Andrey <savitskiy.andrey@gmail.com>
2019-04-13 15:48:51 +0300

    Setting of AlarmManager has been improved taking doze mode restrictions into account.

--- app/src/main/java/com/savitskiy/reminder/alarm/AlarmManagerImpl.kt
+++ app/src/main/java/com/savitskiy/reminder/alarm/AlarmManagerImpl.kt
@@ -1,14 +1,19 @@
 package com.savitskiy.reminder.alarm
 
+import android.annotation.TargetApi
 import android.app.PendingIntent
 import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.savitskiy.reminder.receivers.AlarmReceiver
 
 /**
  * Created by andrey on 09,March,2019
  */
 class AlarmManagerImpl() : AlarmManager {
+
     override fun setAlarm(context: Context, delay: Long) {
+
         val intent = AlarmReceiver.getIntent(context)
 
         val pIntent = PendingIntent.getBroadcast(
@@ -16,7 +21,44 @@ class AlarmManagerImpl() : AlarmManager {
             PendingIntent.FLAG_CANCEL_CURRENT
         )
         val am = context.getSystemService(Context.ALARM_SERVICE) as android.app.AlarmManager
-        am.set(android.app.AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + delay, pIntent)
+        am.set(
+            android.app.AlarmManager.RTC_WAKEUP,
+            System.currentTimeMillis() + delay,
+            pIntent
+        )
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            setAlarmMAndAbove(am, pIntent, delay)
+        } else {
+            setAlarmBelowM(am, pIntent, delay)
+        }
+
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun setAlarmMAndAbove(
+        alarmManager: android.app.AlarmManager,
+        pendingIntent: PendingIntent,
+        delay: Long
+    ) {
+        alarmManager.setAndAllowWhileIdle(
+            android.app.AlarmManager.RTC_WAKEUP,
+            System.currentTimeMillis() + delay,
+            pendingIntent
+        )
+    }
+
+    private fun setAlarmBelowM(
+        alarmManager: android.app.AlarmManager,
+        pendingIntent: PendingIntent,
+        delay: Long
+    ) {
+        alarmManager.set(
+            android.app.AlarmManager.RTC_WAKEUP,
+            System.currentTimeMillis() + delay,
+            pendingIntent
+        )
     }
 
 }
